c40e350bdf5b9f776225ca8c596bc1974438d864
HEMANT <hemant.dhiman92@gmail.com>
2020-02-27 04:06:27 +0530

    stack complete

--- src/com/Data_Structure/Main.java
+++ src/com/Data_Structure/Main.java
@@ -2,6 +2,14 @@ package com.Data_Structure;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Stack!");
+        Stack stack = new Stack(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+
+        for (int i = 0; i <= stack.lenght(); i++) {
+            System.out.println(stack.pop());
+        }
+        System.out.println("stack top: " + stack.top());
     }
 }
---
+++ src/com/Data_Structure/Stack.java
@@ -0,0 +1,37 @@
+package com.Data_Structure;
+
+public class Stack {
+    private int[] stack;
+    private int size;
+
+    Stack(int size) {
+        this.stack = new int[size];
+    }
+
+    void push(int value) {
+        if (this.size <= stack.length) {
+            stack[this.size] = value;
+            this.size++;
+        }
+    }
+
+    int pop() {
+        if (size > 0) {
+            this.size--;
+            return stack[size];
+        }else
+            return -1;
+    }
+
+    int lenght() {
+        return this.size + 1;
+    }
+
+    int top() {
+        if (size - 1 >= 0)
+            return stack[size - 1];
+        else
+            return -1;
+    }
+
+}
