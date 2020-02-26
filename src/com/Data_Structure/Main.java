d5a121a45aa19a2f7bc9978f2861de4d3b0dac64
HEMANT <hemant.dhiman92@gmail.com>
2020-02-27 02:29:13 +0530

    initial commit

--- src/com/Data_Structure/Main.java
+++ src/com/Data_Structure/Main.java
@@ -1,9 +1,7 @@
-package com.company;
+package com.Data_Structure;
 
-public class Main {
-
+class Main {
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Stack!");
     }
 }
