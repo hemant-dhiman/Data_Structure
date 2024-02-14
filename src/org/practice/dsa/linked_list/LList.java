002da87541b1a4d08b575557359d68f2ed9604f2
Hemant Dhiman <hemant.dhiman92@gmail.com>
2024-02-15 01:24:18 +0530

    added ll methods: append, rmLast, prePend, rmFirst, get & set.

--- src/org/practice/dsa/linked_list/LList.java
+++ src/org/practice/dsa/linked_list/LList.java
@@ -34,6 +34,88 @@ public class LList {
         }
     }
 
+    public void append(int value) {
+        LlNode newNode = new LlNode(value);
+
+        if (this.head == null && this.tail == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+    }
+
+    public LlNode removeLast() {
+
+        if (this.head == null) return null;
+
+        LlNode current = this.head;
+        LlNode previous = this.head;
+
+        while (current.next != null) {
+            previous = current;
+            current = current.next;
+        }
+        previous.next = null;
+        this.tail = previous;
+        this.length--;
+
+        if (this.length == 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return current;
+    }
+
+    public void prepend(int value) {
+        LlNode newNode = new LlNode(value);
+        if (this.head == null && this.tail == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+    }
+
+    public LlNode removeFirst() {
+        if (this.head == null && this.tail == null) return null;
+
+        LlNode current = this.head;
+        this.head = this.head.next;
+        current.next = null;
+        this.length--;
+
+        if (this.length == 0) {
+            this.tail = null;
+        }
+        return current;
+    }
+
+    public LlNode get(int index) {
+        if (index < 0 || index >= length) return null;
+
+        LlNode current = this.head;
+
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    public boolean set(int index, int value) {
+        LlNode current = this.get(index);
+        if (current != null) {
+            current.value = value;
+            return true;
+        }
+        return false;
+    }
+
+
     public void getHead() {
         System.out.println("Head: " + head.value);
     }
--- src/org/practice/dsa/linked_list/Main.java
+++ src/org/practice/dsa/linked_list/Main.java
@@ -13,5 +13,56 @@ public class Main {
         linkedList.getLength();
         linkedList.printLList();
 
+        System.out.println();
+
+        LList linkedList2 = new LList(1);
+        linkedList2.append(48);
+        linkedList2.append(46);
+        linkedList2.append(47);
+        System.out.println("Removed Last: " + linkedList2.removeLast().value);
+        linkedList2.getHead();
+        linkedList2.getTail();
+        linkedList2.getLength();
+        linkedList2.printLList();
+
+        System.out.println();
+
+        linkedList2.prepend(100);
+        linkedList2.getHead();
+        linkedList2.getTail();
+        linkedList2.getLength();
+        linkedList2.printLList();
+
+        System.out.println();
+
+        LList testingRemoveFirst = new LList(2);
+        testingRemoveFirst.append(1);
+
+        System.out.println("Removed First: " + testingRemoveFirst.removeFirst().value);
+        testingRemoveFirst.getHead();
+        testingRemoveFirst.getTail();
+        testingRemoveFirst.getLength();
+        testingRemoveFirst.printLList();
+
+        System.out.println();
+
+        LList testingGet = new LList(10);
+        testingGet.append(20);
+        testingGet.append(30);
+        testingGet.append(40);
+        System.out.println("Get: " + testingGet.get(2).value + "\n");
+        testingGet.getHead();
+        testingGet.getTail();
+        testingGet.getLength();
+        testingGet.printLList();
+
+        System.out.println();
+
+        System.out.println("Set 100 at 0: " + testingGet.set(0, 100));
+        System.out.println("Set 100 at 4: " + testingGet.set(4, 100));
+        testingGet.getHead();
+        testingGet.getTail();
+        testingGet.getLength();
+        testingGet.printLList();
     }
 }
